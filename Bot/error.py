58348ba761251f87262c30e89ea5a3d5595b6bf6
Katleho-engineer <katleho.professional@gmail.com>
2024-01-22 14:34:44 +0200

    updated error handling

--- Bot/error.py
+++ Bot/error.py
@@ -18,25 +18,17 @@ def error_response():
 
 def help_me():
     suggestions = """
-    I am a WordBot. I can give you the following information about words:
-        - Definition
-        - Antonym
-        - Synonym
-        - Syllables
-        - Part of 
-        - Pronunciation
-        - Use in a sentence
-        - everything 
-
-    Here are the type of request to make:
-        ~ define ambient
-        ~ Antonym of success
-        ~ Synonym of life
-        ~ Syllables of complicated
-        ~ a wheel is part of
-        ~ pronunciation pf bombastic
-        ~ Use careful in a sentence
-        ~ Give me everything about lemon
+    
+    Key_words : {Define, Antonym, Synonym, Syllables, Part of, Pronunciation, Use in a sentence, everything},
+    example1: define life,
+    example2: antonym of kind,
+    example3: synonym of happy,
+    example4: Syllables of entertainment,
+    example5: a wheel is a part of,
+    example6: Pronunciation of entertainment,
+    example7: Use careful in a sentence,
+    example7: Give me everything about lemon,
+    
      """
     return suggestions
 
